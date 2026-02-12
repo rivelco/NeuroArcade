a9d1599beacde709ba65705b166a28ff644c04a4
rivelco <16997545+rivelco@users.noreply.github.com>
2026-02-12 16:49:21 -0600

    Upgraded expression tracker controller
    Added expressions selector and great accuracy now

--- src/neuroarcade/controls/ExpressionTracker.py
+++ src/neuroarcade/controls/ExpressionTracker.py
@@ -4,6 +4,7 @@ import mediapipe as mp
 
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.components.containers import NormalizedLandmark
 
 from neuroarcade.core.direction import Direction
 from neuroarcade.controls.base import BaseControl
@@ -11,8 +12,23 @@ from neuroarcade.controls.base import BaseControl
 from importlib.resources import files
 
 class ExpressionTracker(BaseControl):
-    def __init__(self):
-        self.cap = cv2.VideoCapture(0)
+    def __init__(self, camera=0,
+                 gest_up = "rise_eyebrows", gest_down = "mouth_open", gest_left = "left_smile", gest_right = "right_smile",
+                 prob_up = 0.4, prob_down = 0.4, prob_left = 0.4, prob_right = 0.4,
+                 draw_lands = True, complete_lands=False, highlight_lands=True):
+        self.cap = cv2.VideoCapture(camera)
+        self.gest_up = gest_up
+        self.gest_down = gest_down
+        self.gest_left = gest_left
+        self.gest_right = gest_right
+        self.prob_up = prob_up
+        self.prob_down = prob_down
+        self.prob_left = prob_left
+        self.prob_right = prob_right
+        self.draw_complete_landmarks = complete_lands
+        self.highlight_lands = highlight_lands
+        self.draw_lands = draw_lands
+        
         model_path = str(files("neuroarcade.assets").joinpath("face_landmarker.task"))
         base_options = python.BaseOptions(
             model_asset_path=model_path
@@ -20,11 +36,10 @@ class ExpressionTracker(BaseControl):
 
         options = vision.FaceLandmarkerOptions(
             base_options=base_options,
-            output_face_blendshapes=False,
+            output_face_blendshapes=True,
             output_facial_transformation_matrixes=False,
             num_faces=1
         )
-        self.mode = "head_pose"
         self.detector = vision.FaceLandmarker.create_from_options(options)
         self.neutral = None
 
@@ -55,56 +70,22 @@ class ExpressionTracker(BaseControl):
 
             if self.neutral is None:
                 self.neutral = (nx, ny)
+                
+            frame = self.paint_landmarks(frame, result.face_landmarks[0])
 
-            dx = nx - self.neutral[0]
-            dy = ny - self.neutral[1]
-
-            thresh = 25
-
-            # ---------------- MODES ----------------
-            if self.mode == "head_pose":
-                if dy < -thresh:
-                    direction = Direction.UP
-                elif dy > thresh:
-                    direction = Direction.DOWN
-                elif dx < -thresh:
-                    direction = Direction.LEFT
-                elif dx > thresh:
-                    direction = Direction.RIGHT
-
-            elif self.mode == "eyebrows":
-                brow = self._get_blend(blends, "browInnerUp")
-                if brow > 0.4:
-                    direction = Direction.UP
-
-            elif self.mode == "mouth":
-                mouth = self._get_blend(blends, "jawOpen")
-                if mouth > 0.5:
-                    direction = Direction.DOWN
-
-            elif self.mode == "smile":
-                left = self._get_blend(blends, "mouthSmileLeft")
-                right = self._get_blend(blends, "mouthSmileRight")
-
-                if left > 0.4:
-                    direction = Direction.LEFT
-                elif right > 0.4:
-                    direction = Direction.RIGHT
-
-            elif self.mode == "gaze":
-                up = self._get_blend(blends, "eyeLookUpLeft")
-                down = self._get_blend(blends, "eyeLookDownLeft")
-                left = self._get_blend(blends, "eyeLookOutLeft")
-                right = self._get_blend(blends, "eyeLookInLeft")
-
-                if up > 0.4:
-                    direction = Direction.UP
-                elif down > 0.4:
-                    direction = Direction.DOWN
-                elif left > 0.4:
-                    direction = Direction.LEFT
-                elif right > 0.4:
-                    direction = Direction.RIGHT
+            gest_up = self._get_blend(blends, self.gest_up)
+            gest_down = self._get_blend(blends, self.gest_down)
+            gest_right = self._get_blend(blends, self.gest_left)
+            gest_left = self._get_blend(blends, self.gest_right)
+            
+            if gest_down > self.prob_down:
+                direction = Direction.DOWN
+            elif gest_left > self.prob_left:
+                direction = Direction.LEFT
+            elif gest_right > self.prob_right:
+                direction = Direction.RIGHT
+            elif gest_up > self.prob_up:
+                direction = Direction.UP
         
         return direction, frame
 
@@ -113,8 +94,157 @@ class ExpressionTracker(BaseControl):
             if b.category_name == name:
                 return b.score
         return 0.0
+    
+    def paint_landmarks(self, frame, landmarks):
+        h, w, _ = frame.shape
+        coords = self.get_landmark_coords(landmarks, w, h)
+        if self.draw_lands:
+            if self.draw_complete_landmarks:
+                for (x, y, _) in coords:
+                    cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                
+                if False:   # Draw the landmarks index, useful for debug
+                    for idx, (x, y, _) in enumerate(coords):
+                        cv2.putText(
+                            frame,
+                            str(idx),
+                            (x, y),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.3,
+                            (0, 255, 0),
+                            1,
+                            cv2.LINE_AA
+                        )
+            else:
+                for i in range(0, len(coords), 2):  # draw every 3rd point
+                    x, y, _ = coords[i]
+                    cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+        if self.highlight_lands:
+            nose = coords[1]
+            cv2.circle(frame, (nose[0], nose[1]), 6, (0, 0, 255), -1)
+            #left_eye = coords[33]
+            #right_eye = coords[263]
+            left_eyebrow = coords[52]
+            right_eyebrow = coords[282]
+            up_lip = coords[11]
+            down_lip = coords[17]
+            smile_left = coords[57]
+            smile_right = coords[432]
+            cv2.circle(frame, (left_eyebrow[0], left_eyebrow[1]), 5, (255, 255, 0), -1)
+            cv2.circle(frame, (right_eyebrow[0], right_eyebrow[1]), 5, (255, 255, 0), -1)
+            cv2.circle(frame, (up_lip[0], up_lip[1]), 5, (255, 255, 0), -1)
+            cv2.circle(frame, (down_lip[0], down_lip[1]), 5, (255, 255, 0), -1)
+            cv2.circle(frame, (smile_left[0], smile_left[1]), 5, (255, 255, 0), -1)
+            cv2.circle(frame, (smile_right[0], smile_right[1]), 5, (255, 255, 0), -1)
+        
+        return frame
+
+    def get_landmark_coords(self, landmarks: list[NormalizedLandmark], width: int, height: int) -> np.ndarray:
+        """Extract normalized landmark coordinates to array of pixel coordinates."""
+        xyz = [(lm.x, lm.y, lm.z) for lm in landmarks]
+        return np.multiply(xyz, [width, height, width]).astype(int)
 
     # -------------------------------------------------
     def get_config_schema(self):
-        return {}
+        available_gestures = [
+            "browDownLeft",     "browDownRight",        "browInnerUp",      "browOuterUpLeft",
+            "browOuterUpRight", "cheekPuff",            "cheekSquintLeft",  "cheekSquintRight",
+            "eyeBlinkLeft",     "eyeBlinkRight",        "eyeLookDownLeft",  "eyeLookDownRight",
+            "eyeLookInLeft",    "eyeLookInRight",       "eyeLookOutLeft",   "eyeLookOutRight",
+            "eyeLookUpLeft",    "eyeLookUpRight",       "eyeSquintLeft",    "eyeSquintRight",
+            "eyeWideLeft",      "eyeWideRight",         "jawForward",       "jawLeft",
+            "jawOpen",          "jawRight",             "mouthClose",       "mouthDimpleLeft",
+            "mouthDimpleRight", "mouthFrownLeft",       "mouthFrownRight",  "mouthFunnel",
+            "mouthLeft",        "mouthLowerDownLeft",   "mouthLowerDownRight", "mouthPressLeft",
+            "mouthPressRight",  "mouthPucker",          "mouthRight",       "mouthRollLower",
+            "mouthRollUpper",   "mouthShrugLower",      "mouthShrugUpper",  "mouthSmileLeft",
+            "mouthSmileRight",  "mouthStretchLeft",     "mouthStretchRight","mouthUpperUpLeft",
+            "mouthUpperUpRight","noseSneerLeft",        "noseSneerRight"
+        ]
+        return {
+            "camera": {
+                "name": "Index of camera device",
+                "description": "The index of camera device",
+                "default": 0,
+                "min": 0,
+                "max": 50
+            },
+            "gest_up": {
+                "name": "Gesture for Up",
+                "description": "Gesture to make to move up",
+                "type": "enum",
+                "options": available_gestures,
+                "default": "browInnerUp",
+            },
+            "gest_down": {
+                "name": "Gesture for Down",
+                "description": "Gesture to make to move down",
+                "type": "enum",
+                "options": available_gestures,
+                "default": "jawOpen",
+            },
+            "gest_left": {
+                "name": "Gesture for Left",
+                "description": "Gesture to make to move left",
+                "type": "enum",
+                "options": available_gestures,
+                "default": "mouthSmileLeft",
+            },
+            "gest_right": {
+                "name": "Gesture for Right",
+                "description": "Gesture to make to move right",
+                "type": "enum",
+                "options": available_gestures,
+                "default": "mouthSmileRight",
+            },
+            "prob_up": {
+                "name": "Threshold for up",
+                "description": "The probability threshold for identifying up expression",
+                "default": 0.4,
+                "min": 0.000001,
+                "max": 1.0,
+            },
+            "prob_left": {
+                "name": "Threshold for left",
+                "description": "The probability threshold for identifying left expression",
+                "default": 0.4,
+                "min": 0.000001,
+                "max": 1.0,
+            },
+            "prob_right": {
+                "name": "Threshold for right smile",
+                "description": "The probability threshold for identifying right expression",
+                "default": 0.4,
+                "min": 0.000001,
+                "max": 1.0,
+            },
+            "prob_down": {
+                "name": "Threshold for mouth",
+                "description": "The probability threshold for identifying down expression",
+                "default": 0.4,
+                "min": 0.000001,
+                "max": 1.0,
+            },
+            "draw_lands": {
+                "name": "Draw face landmarks",
+                "description": "Check to show the landmarks in the face",
+                "default": True,
+                "min": 0,
+                "max": 1,
+            },
+            "complete_lands": {
+                "name": "Draw complete landmarks",
+                "description": "Check for more points in the face, uncheck for faster drawing",
+                "default": False,
+                "min": 0,
+                "max": 1,
+            },
+            "highlight_lands": {
+                "name": "Highlight eyes and nose",
+                "description": "Check to highlight eyes and nose",
+                "default": True,
+                "min": 0,
+                "max": 1,
+            },
+        }
 
